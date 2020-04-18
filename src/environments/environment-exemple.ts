6d740461b575c31f2b72433bac51be31bff70166
TANK <nelsontayou1@gmail.com>
2020-04-18 14:20:29 +0200

    clean code +

---
+++ src/environments/environment-exemple.ts
@@ -0,0 +1,18 @@
+export const environment = {
+    url_prefix: "",
+    url_frontend:"",
+    url_service:"",
+    global_logo:"",
+    drapeau:"",
+    nom:"",
+    indicatif_pays:"fr",
+    path_qgis:"/var/www/geosm/france/",
+    pojet_nodejs:"france",
+    primaryColor:"#1F8FEA",
+    removeFonction:['itineraire'],
+    default_language:"fr",
+    avaible_language:['fr',"en"],
+    projetOsmCm:false,
+    defaultLayers:[],
+    production: false
+  };
