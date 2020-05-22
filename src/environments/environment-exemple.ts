2c6d195c991fab58158a4d2fa30b9a60a0737429
TANK <nelsontayou1@gmail.com>
2020-05-22 14:31:21 +0200

    add environment-exemple

---
+++ src/environments/environment-exemple.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    url_prefix: '',
+    url_frontend:'',
+    url_service:'',
+    global_logo:'',
+    drapeau:'',
+    nom:'',
+    titreInfos:'',
+    indicatif_pays:'',
+    primaryColor:'#f5ab1f',
+    removeFonction:[],
+    avaible_language:['fr','en'],
+    defaultLayers:[],
+    default_language:'fr',
+    projetOsmCm:true,
+    path_qgis:'path_qgis_value',
+    pojet_nodejs:'pojet_nodejs_value',
+    production: false
+  };
