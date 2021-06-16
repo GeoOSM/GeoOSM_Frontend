e7a5dd2c111dbb9498726e9ac6a578d4725839f6
BorisGautier <tchoukouahaboris@outlook.fr>
2021-06-16 15:15:16 +0100

    update link

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,27 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BrowserModule } from "@angular/platform-browser";
+import { Routes, RouterModule } from "@angular/router";
 
-import { HomeComponent } from './home/home.component';
-import { MapComponent } from './map/map.component';
+import { HomeComponent } from "./home/home.component";
+import { MapComponent } from "./map/map.component";
 
-
-const routes: Routes =[
-    // { path: 'home',  component: HomeComponent },
-    { path: 'map',   component: MapComponent },
-    { path: '', redirectTo: 'map', pathMatch: 'full' }
+const routes: Routes = [
+  // { path: 'home',  component: HomeComponent },
+  { path: "geoportail", component: MapComponent },
+  { path: "", redirectTo: "geoportail", pathMatch: "full" },
 ];
 
-@NgModule({ 
-  imports: [
-    CommonModule,
-    BrowserModule,
-    RouterModule.forRoot(routes)
-  ],
-  exports: [
-
-  ],
+@NgModule({
+  imports: [CommonModule, BrowserModule, RouterModule.forRoot(routes)],
+  exports: [],
 })
-export class AppRoutingModule { }
- 
+export class AppRoutingModule {}
